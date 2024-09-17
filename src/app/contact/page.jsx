77c8739d5953bd0ebeb526c5145f9695c45bc1d5
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-09-17 11:42:51 +0500

    tugadi

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { toast } from "react-toastify";
